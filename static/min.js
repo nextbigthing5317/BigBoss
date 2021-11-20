58c4b004f8094d8b6b7b693319df090a048b6956
suryadb102 <69720915+suryadb102@users.noreply.github.com>
2021-11-20 16:08:13 +0530

    Update min.js

--- static/min.js
+++ static/min.js
@@ -1,30 +1,16 @@
-function confettiFalling() {
+const start = () => {
+            setTimeout(function() {
+                confetti.start()
+            }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+        };
 
-	var box = document.getElementById("box");
-	var colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink'];
+        //  for stopping the confetti 
 
-	for (var i = 0; i < 1000; i++) {
-		
-		// Create 1000 DIV elements for confetti
-		var div = document.createElement("div");
-		div.classList.add("confetti");
-		box.appendChild(div);
-	}
-
-	var confetti = document.querySelectorAll('.confetti');
-
-	for (var i = 0; i < confetti.length; i++) {
-		
-		var size = Math.random() * 0.01 * [i];
-
-		confetti[i].style.width = 5 + size + 'px';
-		confetti[i].style.height = 16 + size + 'px';
-		confetti[i].style.left = Math.random() * innerWidth + 'px';
-
-		var background = colors[Math.floor(Math.random() * colors.length)];
-		confetti[i].style.backgroundColor = background;
-
-		box.children[i].style.transform = "rotate("+ size*[i] +"deg)";
-	}
-}
-confettiFalling();
+        const stop = () => {
+            setTimeout(function() {
+                confetti.stop()
+            }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+        };
+// after this here we are calling both the function so it works
+        start();
+        stop();
